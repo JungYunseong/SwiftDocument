e6122cf1e0b24e500c0bb4dd370f603126b0f0ff
JungYunseong <cysseo1@naver.com>
2022-04-28 15:12:17 +0900

    Initial Commit

---
+++ SwiftDocument/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDocument
+//
+//  Created by Jung Yunseong on 2022/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftDocument/SwiftDocumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDocumentApp.swift
+//  SwiftDocument
+//
+//  Created by Jung Yunseong on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDocumentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
