e8c60c1071a78453119330d7e7297323d1e05163
JungYunseong <cysseo1@naver.com>
2022-05-03 16:38:06 +0900

    기능추가: 메모리스트

--- SwiftDocument/SwiftDocumentApp.swift
+++ SwiftDocument/SwiftDocumentApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct SwiftDocumentApp: App {
+    
+    @StateObject var store = MemoStore()
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
--- SwiftDocument/Views/MemoView.swift
+++ SwiftDocument/Views/MemoView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct MemoView: View {
+    
+    @EnvironmentObject var store: MemoStore
+    
     var body: some View {
         ZStack {
             Color("background")
                 .edgesIgnoringSafeArea(.top)
+            
+            NavigationView {
+                List(store.list) {memo in
+                    VStack(alignment: .leading) {
+                        Text(memo.content)
+                            .font(.body)
+                            .lineLimit(1)
+                        
+                        Text(memo.insertDate, style: .date)
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .listStyle(.plain)
+                .navigationBarHidden(true)
+            }
         }
     }
 }
@@ -19,5 +38,6 @@ struct MemoView: View {
 struct MemoView_Previews: PreviewProvider {
     static var previews: some View {
         MemoView()
+            .environmentObject(MemoStore())
     }
 }
