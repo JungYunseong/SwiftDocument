280222fd325ba906d04241a99450c0cd1fa05519
JungYunseong <cysseo1@naver.com>
2022-05-05 16:24:56 +0900

    기타: 프리뷰
    content preview뷰 수정

--- SwiftDocument/ContentView.swift
+++ SwiftDocument/ContentView.swift
@@ -100,5 +100,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(MemoStore())
     }
 }
